3976ae4d0913122bc46982fad5dae5929b9b7c34
Shuhaib9446 <shuhaibpmohammed@gmail.com>
2025-11-26 14:13:20 +0530

    updated allprograms

--- src/components/AllPrograms/Programs.jsx
+++ src/components/AllPrograms/Programs.jsx
@@ -18,19 +18,22 @@ const Programs = () => {
       </div>
       <div className=' grid grid-cols-1 place-items-center gap-0 md:gap-5  md:grid-cols-3 px-[5px] md:px-5 lg:px-[150px] '>
         {designData.map((item, i) => (
-          <div key={i} className=' group md:group-hover:flex md:group-hover:flex-col'>
-            <div className='relative group-hover:h-10 bg-red-900'>
-              <img src={item.img} alt="image" className='rounded-xl object-center group-hover:h-100' onClick={() => handleclick(item.id)} />
-              <p className='absolute bottom-45 left-9 md:bottom-18 md:left-9 text-4xl text-white font-extrabold leading-[1.2] tracking-[-1px] '>{item.heading}</p>
+          <div key={i} className='group md:hover:h-auto md:group-hover:flex md:group-hover:flex-col w-full bg-red900'>
+            <div className='relative md:group-hover:w-full   bg-green700 md:object-cover'>
+              <img src={item.img} alt="image" className='relative w-full rounded-xl object-cover md:group-hover:aspect-square md:group-hover:w-full' onClick={() => handleclick(item.id)} />
+              <p className='absolute md:group-hover:bottom-10 bottom-45 left-9 md:bottom-18 md:left-9 text-4xl text-white font-extrabold leading-[1.2] tracking-[-1px] '>{item.heading}</p>
             </div>
-            <div className='flex md:group-hover:block flex-col gap-4 md:hidden  bg-[#0A4F48] rounded-2xl w-full my-5 py-4 px-4 text-white'>
-              <p className='text-xl'>Duration</p>
-              <div className='flex flex-row justify-between md:hover:block  text-[18px]'>
-                <button className='rounded-2xl border py-2 px-2  bg-[#10635b]'>30 Days</button>
-                <button className='rounded-2xl border py-2 px-2  bg-[#10635b]'>60 Days</button>
-                <button className='rounded-2xl border py-2 px-2  bg-[#10635b]'>90 Days</button>
+
+            <div className='flex md:group-hover:block md:group-hover:h-auto flex-col gap-4 md:hidden  bg-[#0A4F48] rounded-2xl w-full  my-2 py-5 px-5  md:py-2 md:px-2 lg:py-4 lg:px-4 text-white'>
+              <p className='md:text-md text-xl'>Duration</p>
+              <div className='flex flex-row justify-between  md:text-xs lg:text-lg'>
+                <button className='rounded-2xl border px-10 py-2 md:text-xs md:text-md lg-text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>30 Days</button>
+                <button className='rounded-2xl border px-10 py-2 md:text-xs md:text-md lg-text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>60 Days</button>
+                <button className='rounded-2xl border px-10 py-2 md:text-xs md:text-md lg-text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>90 Days</button>
               </div>
             </div>
+
+
           </div>
         ))}
       </div>
