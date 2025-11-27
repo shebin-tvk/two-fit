d8357b536db8f030380b36a6dfe89599e995739c
Shuhaib9446 <shuhaibpmohammed@gmail.com>
2025-11-27 09:15:46 +0530

    updated allprograms

--- src/components/AllPrograms/Programs.jsx
+++ src/components/AllPrograms/Programs.jsx
@@ -24,12 +24,12 @@ const Programs = () => {
               <p className='absolute md:group-hover:bottom-10 bottom-45 left-9 md:bottom-18 md:left-9 text-4xl text-white font-extrabold leading-[1.2] tracking-[-1px] '>{item.heading}</p>
             </div>
 
-            <div className='flex md:group-hover:block md:group-hover:h-auto flex-col gap-4 md:hidden  bg-[#0A4F48] rounded-2xl w-full  my-2 py-5 px-5  md:py-2 md:px-2 lg:py-4 lg:px-4 text-white'>
-              <p className='md:text-md text-xl'>Duration</p>
-              <div className='flex flex-row justify-between  md:text-xs lg:text-lg'>
-                <button className='rounded-2xl border px-10 py-2 md:text-xs md:text-md lg-text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>30 Days</button>
-                <button className='rounded-2xl border px-10 py-2 md:text-xs md:text-md lg-text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>60 Days</button>
-                <button className='rounded-2xl border px-10 py-2 md:text-xs md:text-md lg-text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>90 Days</button>
+            <div className='flex w-full md:group-hover:block  md:group-hover:h-auto flex-col gap-4 md:hidden  bg-[#0A4F48] rounded-2xl   my-2 py-3 px-5  md:py-2 md:px-2 lg:py-4 lg:px-4 text-white'>
+              <p className='md:px-4 md:text-md text-xl lg:px-2'>Duration</p>
+              <div className='flex flex-row  gap-2 md:gap-5 md:py-2 px-3 md:px-3 md:text-lg lg:text-2xl lg:gap-8 lg:px-2 lg:py-1'>
+                <button className='rounded-2xl border px-5  py-1 md:text-xs  md:text-md lg:text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>30 Days</button>
+                <button className='rounded-2xl border px-5  py-1 md:text-xs  md:text-md lg:text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>60 Days</button>
+                <button className='rounded-2xl border px-5  py-1 md:text-xs  md:text-md lg:text-lg md:py-1 lg:py-2 md:px-1 lg:px-2  bg-[#10635b]'>90 Days</button>
               </div>
             </div>
 
